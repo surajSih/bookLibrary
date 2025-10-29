ec227abbf1af21d51ecf346909a936bfa6621f77
surajSih <surajsingh93958@gmail.com>
2025-10-29 21:17:06 +0530

    fixed hai bro check kar

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  images: {
+    domains: ["covers.openlibrary.org"],
+  },
 };
 
 export default nextConfig;
