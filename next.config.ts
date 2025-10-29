c001682287f8a48463ff2dd9ee2eff95aaa36512
surajSih <surajsingh93958@gmail.com>
2025-10-29 21:04:39 +0530

    fixed

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["covers.openlibrary.org"],
-  },
+  
 };
 
 export default nextConfig;
